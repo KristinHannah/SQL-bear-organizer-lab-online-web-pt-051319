d6ee58122b34f3013556558d70b262ce8004a0fb
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-20 23:23:08 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,7 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT "
+  "SELECT name, age FROM bears WHERE gender = f"
 end
-
+SELECT name FROM cats
 def selects_all_bears_names_and_orders_in_alphabetical_order
   "Write your SQL query here"
 end
