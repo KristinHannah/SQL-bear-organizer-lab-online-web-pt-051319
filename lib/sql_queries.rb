5c23856fd707db6e2e66143d9e8ae6458375711c
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-20 23:31:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,8 +19,10 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT "
+  "SELECT COUNT(color)"
 end
+
+ "SELECT COUNT([column name]) FROM [table name] WHERE [column name] = [value]"
 
 def counts_number_of_bears_with_goofy_temperaments
   "Write your SQL query here"
